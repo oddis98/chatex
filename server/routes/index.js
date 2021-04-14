e9599204f9a3cf992201007de5e7cd528db8774c
oddis98 <oddis98@live.no>
2021-04-15 00:06:12 +0200

    updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -36,6 +36,8 @@ router.post("/login/:userId", encode, async (req, res, next) => {
   req.session.userId = existingUser._id;
   req.session.sessionid = req.sessionID;
 
+  req.session.save();
+
   return res.status(200).json({
     success: true,
     email: req.session.email,
@@ -46,7 +48,7 @@ router.post("/login/:userId", encode, async (req, res, next) => {
 router.get("/login", async (req, res) => {
   try {
     console.log(req);
-    const sess = req.session.email;
+    const sess = req.session;
     // mongoose.connection.db.collection(
     //   "sessions",
     //   async function (err, collection) {
