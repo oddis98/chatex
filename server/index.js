1ffc95462b3f9349785bda48cec1a5414c6deac0
oddis98 <oddis98@live.no>
2021-04-15 11:09:12 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -58,7 +58,10 @@ app.use(
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://thelunarproject.asuscomm.com/chatex_client/home"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
