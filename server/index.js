f47bf0c7f878b961b561ee2e25bf242edb55b556
oddis98 <oddis98@live.no>
2021-04-15 11:06:33 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -58,10 +58,7 @@ app.use(
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://thelunarproject.asuscomm.com"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
